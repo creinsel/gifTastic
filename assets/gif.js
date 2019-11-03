4213d27584e25d3bbde92873a62803a366edff10
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-11-03 14:17:17 -0800

    click animate, added classes and ids

--- assets/gif.js
+++ assets/gif.js
@@ -1,9 +1,11 @@
 // gif=$(this).attr("data-name")
 var cartArr=["Spongebob", "Family Guy", "Rugrats", "Teen Titans",  "American Dad"]
-var gif="grinch";
+var userInput=$("#input-form").val();
+var gif="grinch"
 var gifAPI="3eDaBXUjJfvyaSnSZueu1CqEJC5iZLdQ";
 var queryURL="http://api.giphy.com/v1/gifs/search?q="+gif+ "&api_key="+gifAPI+ "&limit=10";
 
+//to create buttons already in array
 function renderButtons(){
 
   $("#buttons").empty();
@@ -16,12 +18,13 @@ function renderButtons(){
   })
 }
 
-  
+
+ //user adding buttons 
 $("#input-btn").on("click", function(event){
 
   event.preventDefault();
 
-  var userInput=$("#input-form").val();
+  userInput=$("#input-form").val();
 
   console.log(userInput);
 
@@ -38,9 +41,69 @@ $("#input-btn").on("click", function(event){
 
 renderButtons();
 
+
+
+//call
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
       console.log(response)
-    })
+
+      //to store data that will be used in variable
+      var results=response.data;
+
+      console.log(results);
+
+      //looping through each result item
+      for (let i = 0; i < results.length; i++) {
+        
+        //div for gifs
+        var cartDiv= $("<div>");
+
+        //variable to display rating from results
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        //img tag
+        var cartImg= $("<img>").addClass("gif");
+
+        //still gif
+        var stillGif= results[i].images.fixed_height_still.url;
+
+        //animated gif
+        var animGif=results[i].images.fixed_height.url;
+
+        //add still and animated attributes
+        cartImg.attr("data-still",stillGif ).attr("data-animate", animGif);
+
+        cartImg.attr("src", stillGif).attr("data-state", "still");
+
+        // adding rating to cartDiv
+        cartDiv.append(p);
+         
+        //adding gifs to cartDiv
+        cartDiv.append(cartImg);
+
+        //adding cartDiv to gifs on the html
+        $("#gifs").prepend(cartDiv);
+        
+      }
+    });
+
+    //animate gifs
+    $("#gifs").on("click", ".gif", function(event){
+     
+      event.preventDefault();
+
+      var state=$(this).attr("data-state");
+
+      if(state==="still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      }
+      else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+
+    });
